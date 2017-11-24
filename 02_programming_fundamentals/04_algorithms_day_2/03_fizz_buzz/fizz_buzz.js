d91d60b65759135f653ed978f4c34cc3abab13d2
Martin Delobel <martin.delobel@decathlon.com>
2017-11-24 15:13:46 +0100

    motherfucking fizzbuzz

--- 02_programming_fundamentals/04_algorithms_day_2/03_fizz_buzz/fizz_buzz.js
+++ 02_programming_fundamentals/04_algorithms_day_2/03_fizz_buzz/fizz_buzz.js
@@ -7,10 +7,48 @@
    - if the number is divisible by 3 and 5 : `FizzBuzz`
    - otherwise, the value is preserved.
 */
+function range(startNumber, endNumber) {
+  let array = [];
+  if (startNumber < endNumber) {
+    for (let i = startNumber; i <= endNumber; i++) {
+      array.push(i);
+    }
+    return array;
+  } else {
+    for (let i = startNumber; i >= endNumber; i--) {
+      array.push(i);
+    }
+    return array;
+  }
+}
+
+function isDivisibleBy3(numberIn) {
+  return Number.isInteger(numberIn / 3);
+}
+function isDivisibleBy5(numberIn) {
+  return Number.isInteger(numberIn / 5);
+}
+
 
 function fizzBuzz(list) {
-
+  let newArray = [];
+  for (let i = 0; i < list.length; i++) {
+    if (isDivisibleBy3(list[i]) && isDivisibleBy5(list[i])) {
+      newArray.push("FizzBuzz");
+    } else if (isDivisibleBy3(list[i])) {
+      newArray.push("Fizz");
+    } else if (isDivisibleBy5(list[i])) {
+      newArray.push("Buzz");
+    } else {
+      newArray.push(list[i]);
+    }
+  }
+  console.log(newArray);
+  return newArray;
 }
+
+fizzBuzz(range(1,20));
+
 
 
 module.exports = fizzBuzz;
