1e7758f811f597285ab213b2354b971023126c44
Martin Delobel <martin.delobel@decathlon.com>
2017-11-24 16:41:20 +0100

    yessss

--- 02_programming_fundamentals/04_algorithms_day_2/06_fizz_buzz_with_map/fizz_buzz_map.js
+++ 02_programming_fundamentals/04_algorithms_day_2/06_fizz_buzz_with_map/fizz_buzz_map.js
@@ -8,10 +8,32 @@
 
    YOU MUST USE array.map
 */
-
-function fizzBuzz(list) {
-
+function isDivisibleBy3(numberIn) {
+  return Number.isInteger(numberIn / 3);
 }
 
+function isDivisibleBy5(numberIn) {
+  return Number.isInteger(numberIn / 5);
+}
 
+function fizzBuzzTest(numberIn) {
+  if (isDivisibleBy3(numberIn) && isDivisibleBy5(numberIn)) {
+    return "FizzBuzz";
+  } else if (isDivisibleBy3(numberIn)) {
+    return "Fizz";
+  } else if (isDivisibleBy5(numberIn)) {
+    return "Buzz";
+  } else {
+    return numberIn;
+  }
+}
+
+function fizzBuzz(list) {
+  let newArray = [];
+  newArray = list.map(fizzBuzzTest);
+  console.log(newArray);
+  return newArray;
+}
+
+fizzBuzz([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]);
 module.exports = fizzBuzz;
