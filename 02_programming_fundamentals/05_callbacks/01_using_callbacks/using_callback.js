fadbd2ede42fe293d94782c2250cdbbd20e5242d
Martin Delobel <martin.delobel@decathlon.com>
2017-11-27 14:11:07 +0100

    exo2 done

--- 02_programming_fundamentals/05_callbacks/01_using_callbacks/using_callback.js
+++ 02_programming_fundamentals/05_callbacks/01_using_callbacks/using_callback.js
@@ -1,8 +1,15 @@
 const thisIsThePlayer = (callback) => {
-  const player = { name: "Spartacus", life: 100, strength: 100, agility: 100 };
+  const player = {
+    name: "Spartacus",
+    life: 100,
+    strength: 100,
+    agility: 100
+  };
   callback(player);
-}
+};
 
 // Write a function sayHello that greets: "Hello, Spartacus". (Where "Spartacus" is the player name)
 
-thisIsThePlayer(sayHello);
+thisIsThePlayer(function sayHello(player) {
+  console.log(`Hello, ${player.name}`);
+});
--- 02_programming_fundamentals/05_callbacks/02_create_callbacks/create_callback.js
+++ 02_programming_fundamentals/05_callbacks/02_create_callbacks/create_callback.js
@@ -2,8 +2,9 @@ const log = (text) => console.log(text);
 
 // A function that takes two parameters, this first one being an object like below
 // and the second one being a callback function.
-const getInput = (____, ____) => {
+const getInput = (objectInput, cb) => {
   // When finished, we want to log the key params...
+  cb(objectInput.key);
 };
 
 getInput({user: "Lorem", key: "arrow_up"}, log);
