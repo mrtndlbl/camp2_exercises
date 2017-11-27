7c23c6ed777566bd9fc84261d402ea76faf2819d
Martin Delobel <martin.delobel@decathlon.com>
2017-11-27 14:44:04 +0100

    exo 3 done

--- 02_programming_fundamentals/05_callbacks/03_square_digits/square_digits.js
+++ 02_programming_fundamentals/05_callbacks/03_square_digits/square_digits.js
@@ -6,10 +6,26 @@
 //
 // Note: This is not just the square of a number but the square of each digits
 // Note: The function accepts an integer and returns an integer
+function toSquare (number) {
+  return number * number;
+}
 
 function squareDigits(number) {
   // Your code here
+  const stringNumber = number.toString();
+
+  let stringNumberArray = [];
+  for (let i = 0; i < stringNumber.length; i++) {
+    stringNumberArray.push(stringNumber.charAt(i));
+  }
+
+  const toSquareArray = stringNumberArray.map(toSquare);
+  const newString = toSquareArray.join('');
+  return parseInt(newString, 10)
+
 }
+
+squareDigits(9129);
 
 // Do not remove last lines, it is for tests
 // eslint-disable-next-line
