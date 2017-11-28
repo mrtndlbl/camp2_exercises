f403c29c23a699cc4781863d111eb5c336e4982c
Martin Delobel <martin.delobel@decathlon.com>
2017-11-28 11:04:20 +0100

    Presque OK

--- 02_programming_fundamentals/05_callbacks/06_sort_an_array/sort_an_array.js
+++ 02_programming_fundamentals/05_callbacks/06_sort_an_array/sort_an_array.js
@@ -6,10 +6,31 @@
 //
 // Note: You should not use Array.sort()
 
+let unsortedArray = [24, 7, 9, 72, -8];
 function sort(unsortedArray) {
   // Your code here
+  let i = 0;
+  //Comparaison de 2 caractères consécutifs dans le tableau
+  while (i < unsortedArray.length - 1) {
+    if (unsortedArray[i+1] !== undefined && isSmallerThanNext(unsortedArray[i],unsortedArray[i+1])) {
+      //Si Number [i] inférieur à [i+1] on ne change pas le tableau, et on incrémente la boucle
+      console.log("no change in array");
+      i++;
+    } else {
+      //Sinon on inverse les 2 numéros dans le tableau
+      let smallNumber = unsortedArray[i];
+      unsortedArray[i] = unsortedArray[i+1];
+      unsortedArray[i+1] = smallNumber;
+      console.log(unsortedArray[i],unsortedArray[i+1]);
+    }
+  }
+  console.log(unsortedArray);
 }
 
+function isSmallerThanNext(numberA, numberB) {
+  return numberA < numberB;
+}
+sort(unsortedArray);
 // Do not remove last lines, it is for tests
 // eslint-disable-next-line
 module.exports = sort;
