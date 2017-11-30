112d520c52cd1729c0094cfd00044c32a0fd8836
Martin Delobel <martin.delobel@decathlon.com>
2017-11-30 11:55:31 +0100

    La voiture aussi

--- 02_programming_fundamentals/08_this_day_1/02_chaining/02_car.js
+++ 02_programming_fundamentals/08_this_day_1/02_chaining/02_car.js
@@ -1,3 +1,24 @@
 // See Sparta courses for the exercise summary
+const car = {
+  speed: 0,
+  distance: 0,
+  start: function() {
+    this.speed = 0;
+    this.distance = 0;
+    return this
+  },
+  changeSpeed: function(newSpeed) {
+    this.speed = newSpeed;
+    return this;
+  },
+  drive: function(newMinutes) {
+    // this.minutes = newMinutes;
+    this.distance += this.speed / 60 * newMinutes ;
+    return this;
+  },
+  showDistance: function() {
+    return console.log(`${this.distance} Km`);
+  }
+}
 
 module.exports = car;
