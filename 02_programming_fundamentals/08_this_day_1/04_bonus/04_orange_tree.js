0ce7b437d7c277c86c00aa769de2882ff4f81f30
Martin Delobel <martin.delobel@decathlon.com>
2017-11-30 14:15:40 +0100

    BAM TON ORANGIER

--- 02_programming_fundamentals/08_this_day_1/04_bonus/04_orange_tree.js
+++ 02_programming_fundamentals/08_this_day_1/04_bonus/04_orange_tree.js
@@ -1,3 +1,55 @@
 // See Sparta courses for the exercise summary
+const orangeTree = {
+  height: 0,
+  age: 0,
+  oranges: 43,
+  alive: true,
+  pickAnOrange: function() {
+    if (this.oranges > 0) {
+      this.oranges -= 1;
+      return true;
+    } else {
+      return false;
+    }
+  },
+  ageOneYear: function() {
+    console.log(`orangeTree\'s age = ${this.age}`);
+    console.log(`alive = ${this.alive}`);
+    this.age += 1;
+    if (this.age >= 5 && this.age < 10) {
+      this.oranges = 10;
+      this.height += 25;
+      return this;
+    } else if (this.age < 10) {
+      this.height += 25;
+      return this;
+    } else if (this.age < 20) {
+      this.oranges = 20;
+      this.height += 10;
+      return this;
+    } else if (this.age < 40) {
+      this.oranges = 5
+      return this;
+    } else if (this.age >= 40 && this.age < 50 ) {
+      this.oranges = 0;
+      return this;
+    } else if (this.age >= 50 && this.age <= 100) {
+      this.alive = Math.random() >= 0.5;
+      return this;
+    } else if (this.age > 100) {
+      this.alive = false;
+      return this;
+    } else {
+      return this;
+    }
+  },
+  seed: function() {
+    this.alive = true;
+    this.age = 0;
+    this.oranges = 0;
+    this.height = 0;
+    return this;
+  }
+}
 
 module.exports = orangeTree;
