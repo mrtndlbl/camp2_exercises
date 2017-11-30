3b91531ab47fae409ce48c1b2b345ba8d9c365c6
Martin Delobel <martin.delobel@decathlon.com>
2017-11-30 11:53:51 +0100

    La machine a café est géechar

--- 02_programming_fundamentals/08_this_day_1/03_implementing/03_coffee_machine.js
+++ 02_programming_fundamentals/08_this_day_1/03_implementing/03_coffee_machine.js
@@ -1,6 +1,29 @@
 // See Sparta courses for the exercise summary
 
 // Coffee Machine usage. Insert your code above this comment
+const machine = {
+  litersOfCoffee: 0,
+  fillWithLitersOfCoffee: function(newLitersOfCoffee) {
+    this.litersOfCoffee += newLitersOfCoffee;
+    return this;
+  },
+  expresso: function() {
+    if (this.litersOfCoffee >= 0.08) {
+      this.litersOfCoffee -= 0.08;
+      return true;
+    } else {
+      return false;
+    }
+  },
+  longCoffee: function() {
+    if (this.litersOfCoffee >= 0.15) {
+      this.litersOfCoffee -= 0.15;
+      return true;
+    } else {
+      return false;
+    }
+  }
+};
 
 machine.fillWithLitersOfCoffee(10);
 console.log(machine.expresso()) // => true
