8121ddd374461eeef8f36972b625c8b46dfe2c0c
Martin Delobel <martin.delobel@decathlon.com>
2018-01-11 14:53:22 +0100

    test with map in calculateWinner function

--- 06_react_redux/03_introducing_react/tictactoe/src/App.js
+++ 06_react_redux/03_introducing_react/tictactoe/src/App.js
@@ -60,6 +60,7 @@ class Game extends Component {
     const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
+
     if (calculateWinner(squares) || squares[i]) {
       return;
     }
@@ -128,13 +129,19 @@ function calculateWinner(squares) {
     [0, 4, 8],
     [2, 4, 6]
   ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
+
+  lines.map((line, index) => {
+    const [a, b, c] = lines[index];
+
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      console.log(squares[a]);
       return squares[a];
     }
-  }
+  });
+
   return null;
 }
+
+
 
 export default Game;
